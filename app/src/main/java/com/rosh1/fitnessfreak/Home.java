8443ea7d550968c82e07e00c8c678aaed28582ff
Rosh145 <markrodney145@gmail.com>
2018-06-26 06:57:11 +0300

    Added the Gym Locations Activity and Maps

--- app/src/main/java/com/rosh1/fitnessfreak/Home.java
+++ app/src/main/java/com/rosh1/fitnessfreak/Home.java
@@ -1,13 +1,27 @@
 package com.rosh1.fitnessfreak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+    Button btnGymLocationsHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnGymLocationsHome = findViewById(R.id.btnGymLocationsHome);
+
+        btnGymLocationsHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, GymLocations.class);
+                startActivity(intent);
+            }
+        });
     }
 }
